c9e876f268a1da3a9473a46a5755e1973d03899c
이택 <itaek@itaeg-ui-MacBookAir.local>
2025-07-30 16:06:44 +0900

    n+1 문제 해결법 : Fetch Join, EntityGraph사용

--- src/main/java/com/example/spring/ProductRepository.java
+++ src/main/java/com/example/spring/ProductRepository.java
@@ -1,9 +1,19 @@
 package com.example.spring;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Optional<Product> findByName(String name);
+
+//    @Query("SELECT p FROM Product p JOIN p.wishLists")
+//    List<Product> findAll();
+
+
+//    @EntityGraph(attributePaths = "wishLists")
+//    List<Product> findAll();
 }
